fc7209a001b15e71eb2568e0fc486901f33ecccc
David-Jacks <djackfaith0.5@gmail.com>
2023-11-15 11:30:10 +0000

    python exercises

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,7 @@
 #creating a dictionary
 
 phonebook = {'chris':'555-111', 'katie': '555-2222', 'Joanne':'555-3333'} #this is a dictionary
+if 'Joanner' not in phonebook:
+    print('there is no such key as Joanner')#accessing a value in a the phonebook dictionary using a key
+else :
+    print(None)
