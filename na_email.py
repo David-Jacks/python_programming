a98598967678a81b22091e7705896df12dc8877a
David-Jacks <djackfaith0.5@gmail.com>
2023-11-18 11:21:14 +0000

    python exercises

--- na_email.py
+++ na_email.py
@@ -1,10 +1,17 @@
 #A program on name and Email addresses
 import pickle #pickle to be able to store my dictionary to a file
+
 #global variables
 users_details = {}
 user_choice = 0
 email = ''
 name = ''
+#unpickling from my file
+my_in_file = open('user_details.txt', 'rb')
+
+if my_in_file.peek(1): #this is to check if the file has atleast 1 byte(if its not empty)
+    users_details = pickle.load(my_in_file)
+    my_in_file.close()
 #displaying menu to allow for input taking
 print("Enter any of the bellow choices: ")
 while (user_choice != 5):
@@ -17,15 +24,21 @@ while (user_choice != 5):
         name = input("Enter users name: ")
         email = input("Enter users email address: ")
         users_details[name] = email
+        print("Done!")
     elif (user_choice == 3):
         name = input("Enter users name: ")
         email = input("Enter new email address: ")
         users_details[name] = email
+        print("Done!")
     elif (user_choice == 4):
         name = input("Enter users name to delete user details: ")
         del users_details[name]
+        print("Done!")
     else :
         print("No valid choice made, please check your input")
 
 #saving it to a file
+myfile = open('user_details.txt', 'wb')
+pickle.dump(users_details, myfile)
+myfile.close()
 print(users_details)
